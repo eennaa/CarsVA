84e329b676d797b3d99e928f96dc26b5de13bd74
GojkoMaric <gojko.maric@yahoo.com>
2018-07-13 20:59:37 +0200

    Treci zadatak

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Aleksandra';
+    $age = '27';
+    return view('welcome', compact('name', 'age'));
 });
 
 Route::get('/about', function () {
